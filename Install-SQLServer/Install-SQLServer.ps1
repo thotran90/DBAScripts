99919305b5d3b25ab27b94c70f5c0549e667a9e3
Tho Tran <trandev90@gmail.com>
2023-11-18 23:55:46 +0700

    Finished the first version of script install-sqlserver silently. Tested in Windows 11 - SQL Server Developer Edition

--- Install-SQLServer/Install-SQLServer.ps1
+++ Install-SQLServer/Install-SQLServer.ps1
@@ -8,29 +8,27 @@
 .NOTES
     Version: 1.0
 .LINK
-    https://github.com/thotran90/DBAScripts/blob/main/README.md
+    https://github.com/thotran90/DBAScripts/blob/master/Install-SQLServer/README.md
+    https://www.microsoft.com/en-us/sql-server/sql-server-downloads
 #>
 param(
     # Path to ISO Image, if empty and current directory contains single ISO file, it will be used
     [string] $IsoImg = $Env:SQLSERVER_ISOPATH,
+
+    # Path to log file
+    [string] $InstallingLogDir, # = D:\Logs\
 
     # SQL Server features, see https://learn.microsoft.com/en-us/sql/database-engine/install-windows/install-sql-server-from-the-command-prompt?view=sql-server-ver16#Feature
     # Installing SQL Engine only by default
     [ValidateSet('SQL', 'SQLEngine', 'Replication', 'FullText', 'DQ', 'PolyBase', 'AdvancedAnalytics', 'AS', 'RS', 'DQC', 'IS', 'MDS', 'SQL_SHARED_MR', 'Tools', 'BC', 'BOL', 'Conn', 'DREPLAY_CLT', 'SNAC_SDK', 'SDK', 'LocalDB')]
     [string[]] $Features = @('SQLEngine'),
 
-    # Specifies a nondefault installation directory
-    [string] $InstallDir,
-
-    # Data directory, by default "$Env:ProgramFiles\Microsoft SQL Server"
-    [string] $DataDir,
-
     # Service name. Mandatory, by default MSSQLSERVER
     [ValidateNotNullOrEmpty()]
     [string] $InstanceName = 'MSSQLSERVER',
 
     # sa user password. If empty, SQL security mode (mixed mode) is disabled
-    [SecureString] $SaPassword = "P@ssw0rd",
+    [string] $SaPassword,
 
     # Username for the service account, see https://learn.microsoft.com/en-us/sql/database-engine/install-windows/install-sql-server-from-the-command-prompt?view=sql-server-ver16
     # Optional, by default 'NT Service\MSSQLSERVER'
@@ -38,13 +36,13 @@ param(
 
     # Password for the service account, should be used for domain accounts only
     # Mandatory with ServiceAccountName
-    [SecureString] $ServiceAccountPassword,
+    [string] $ServiceAccountPassword,
 
     # List of system administrative accounts in the form <domain>\<user>
     # Mandatory, by default current user will be added as system administrator
     [string[]] $SystemAdminAccounts = @("$Env:USERDOMAIN\$Env:USERNAME"),
 
-    # Product key, if omitted, evaluation is used unless VL edition which is already activated
+    # Product key
     [string] $ProductKey,
 
     # Use bits transfer to get files from the Internet
@@ -53,3 +51,98 @@ param(
     # Enable SQL Server protocols: TCP/IP, Named Pipes
     [switch] $EnableProtocols
 )
+$ErrorActionPreference = 'STOP'
+$scriptName = (Split-Path -Leaf $PSCommandPath).Replace('.ps1','')
+
+$start = Get-Date
+if(!$InstallingLogDir){
+    $InstallingLogDir = "$PSScriptRoot\$scriptName-$($start.ToString('s').Replace(':','-')).log"
+}else{
+    $InstallingLogDir = "$InstallingLogDir$scriptName-$($start.ToString('s').Replace(':','-')).log"
+}
+Write-Host "Installing log: $InstallingLogDir"
+Start-Transcript $InstallingLogDir
+
+if(!$IsoImg){
+    Write-Host "Iso Image Path is not specified. Please download iso image by following the link:  https://www.microsoft.com/en-us/sql-server/sql-server-downloads"
+    Write-Host "Exiting...."
+    exit 404
+}
+
+Write-Host "Starting install MS SQL Server using ISO image: $IsoImg"
+$volume = Mount-DiskImage $IsoImg -StorageType ISO -PassThru | Get-Volume
+$iso_drive = if($volume){
+    $volume.DriveLetter + ':'
+} else{
+    Get-PSDrive | ? Description -like 'sql*' | % Root
+}
+if(!$iso_drive) {throw "Can't find mounted ISO drive"} else {Write-Host "Mounted drive: $iso_drive"}
+
+Get-ChildItem $iso_drive | ft -auto | Out-String
+Get-CimInstance win32_process | ? { $_.CommandLine -like 'setup.exe*/ACTION=install*'} | % {
+    Write-Host "Sql Server installer is already running, killing it:" $_.Path  "pid: " $_.ProcessId
+    Stop-Process $_.ProcessId -Force
+}
+
+# Setting up the command to install
+$cmd =@(
+    "${iso_drive}setup.exe"
+    '/Q'                                # Silent install
+    '/INDICATEPROGRESS'                 # Specifies that the verbose Setup log file is piped to the console
+    '/IACCEPTSQLSERVERLICENSETERMS'     # Must be included in unattended installations
+    '/ACTION=install'                   # Required to indicate the installation workflow
+    '/UPDATEENABLED=false'              # Should it discover and include product updates.
+
+    "/FEATURES=" + ($Features -join ',')
+
+    #Security
+    "/SQLSYSADMINACCOUNTS=""$SystemAdminAccounts"""
+    '/SECURITYMODE=SQL'                 # Specifies the security mode for SQL Server. By default, Windows-only authentication mode is supported.
+    "/SAPWD=""$SaPassword"""            # Sa user password
+
+    "/INSTANCENAME=$InstanceName"       # Server instance name
+
+    "/SQLSVCACCOUNT=""$ServiceAccountName"""
+    "/SQLSVCPASSWORD=""$ServiceAccountPassword"""
+
+    # Service startup types
+    "/SQLSVCSTARTUPTYPE=automatic"
+    "/AGTSVCSTARTUPTYPE=automatic"
+    "/ASSVCSTARTUPTYPE=manual"
+
+    "/PID=$ProductKey"
+)
+
+# remove empty arguments
+$cmd_out = $cmd = $cmd -notmatch '/.+?=("")?$'
+
+# show all parameters but remove password details
+Write-Host "Install parameters:`n"
+'SAPWD', 'SQLSVCPASSWORD' | % { $cmd_out = $cmd_out -replace "(/$_=).+", '$1"****"' }
+$cmd_out[1..100] | % { $a = $_ -split '='; Write-Host '   ' $a[0].PadRight(40).Substring(1), $a[1] }
+Write-Host "$cmd_out"
+Invoke-Expression "$cmd"
+if ($LastExitCode) {
+    if ($LastExitCode -ne 3010) { throw "SqlServer installation failed, exit code: $LastExitCode" }
+    Write-Warning "SYSTEM REBOOT IS REQUIRED"
+}
+
+if ($EnableProtocols) {
+    function Enable-Protocol ($ProtocolName) { $sqlNP | ? ProtocolDisplayName -eq $ProtocolName | Invoke-CimMethod -Name SetEnable }
+
+    Write-Host "Enable SQL Server protocols: TCP/IP, Named Pipes"
+
+    $sqlCM = Get-CimInstance -Namespace 'root\Microsoft\SqlServer' -ClassName "__NAMESPACE"  | ? name -match 'ComputerManagement' | Select-Object -Expand name
+    $sqlNP = Get-CimInstance -Namespace "root\Microsoft\SqlServer\$sqlCM" -ClassName ServerNetworkProtocol
+
+    Enable-Protocol 'TCP/IP'
+    Enable-Protocol 'Named Pipes'
+
+    Get-Service $InstanceName | Restart-Service -Force
+}
+
+"`nInstallation length: {0:f1} minutes" -f ((Get-Date) - $start).TotalMinutes
+
+Dismount-DiskImage $IsoImg
+Stop-Transcript
+trap { Stop-Transcript; if ($IsoImg) { Dismount-DiskImage $IsoImg -ErrorAction 0 } }
--- Install-SQLServer/README.md
+++ Install-SQLServer/README.md
@@ -1,8 +1,8 @@
 # Install-SQLServer
 
-[![](https://img.shields.io/badge/version-1.0-blue)](https://github.com/thotran90/DBAScripts)
+[![](https://img.shields.io/badge/version-0.1-blue)](https://github.com/thotran90/DBAScripts)
 
-This script installs MS SQL Server on Window OS silently from ISO image that can be available locally or downloaded from the internet.  
+This script installs MS SQL Server on Window OS silently from ISO image that can be available locally.  
 
 The script lists paramerters provided to the native setup but hides sensitve data.  
 ## Prerequisites  
